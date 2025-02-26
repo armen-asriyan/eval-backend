c15c5e8d28fd3a71e31b0f9b69e812d4da6b025e
armen-asriyan <armenasriyan@proton.me>
2025-02-26 11:05:36 +0100

    added confirmation prompt (modal)  before the deletion

--- frontend/src/components/modal/EditModal.jsx
+++ frontend/src/components/modal/EditModal.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import "./EditModal.css";
 import {
   RiAddBoxLine,
+  RiAlertFill,
   RiCloseLine,
   RiDeleteBin6Line,
   RiSave3Line,
@@ -27,6 +28,9 @@ const EditModal = ({ closeModal, skill, skillCategories, skillLevels }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+
+  // Check if user clicked the delete button
+  const [isDeleting, setIsDeleting] = useState(false);
 
   // Handle text input changes
   const handleInputChange = (event) => {
@@ -80,6 +84,11 @@ const EditModal = ({ closeModal, skill, skillCategories, skillLevels }) => {
     }
   };
 
+  // Function to show buttons to confirm or cancel the deletion
+  const handleDeleteConfirm = (shownState) => {
+    setIsDeleting(shownState);
+  };
+
   // Function to delete the skill
   const handleDeleteSubmit = async (event) => {
     event.preventDefault();
@@ -106,8 +115,9 @@ const EditModal = ({ closeModal, skill, skillCategories, skillLevels }) => {
   return (
     <div className="edit-modal-bg">
       <div className="edit-modal-content">
-        <h2>{skill ? "Modifier la" : "Ajouter une"} compétence</h2>
-
+        <h2 className="edit-modal__title">
+          {skill ? "Modifier la" : "Ajouter une"} compétence
+        </h2>
         <form className="skill-form" onSubmit={handleSubmit}>
           <label htmlFor="skill-title">
             Nom de la compétence:
@@ -193,22 +203,59 @@ const EditModal = ({ closeModal, skill, skillCategories, skillLevels }) => {
             <RiCloseLine />
           </button>
         </form>
-
+        {/* If user hasn't yet clicked on the delete button */}
         {skill && (
           <button
             type="button"
-            onClick={handleDeleteSubmit}
             className="edit-modal__btn delete"
+            onClick={() => handleDeleteConfirm(true)}
             disabled={loading}
           >
             <RiDeleteBin6Line /> Supprimer
           </button>
         )}
-
+        {isDeleting && (
+          <div className="edit-modal-bg">
+            <div className="edit-modal-content confirmation">
+              <h2 className="edit-modal__title">
+                Confirmation de la suppression
+              </h2>
+              <p className="edit-modal__confirm-delete-text">
+                Êtes-vous sûr de vouloir supprimer cette compétence ?
+                <RiAlertFill />
+              </p>
+              <div className="edit-modal__confirm-delete">
+                <button
+                  type="button"
+                  className="edit-modal__btn delete"
+                  onClick={handleDeleteSubmit}
+                  disabled={loading}
+                >
+                  <RiDeleteBin6Line /> Supprimer
+                </button>
+                <button
+                  type="button"
+                  className="edit-modal__btn cancel-delete"
+                  onClick={() => handleDeleteConfirm(false)}
+                  disabled={loading}
+                >
+                  <RiCloseLine /> Annuler
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteConfirm(false)}
+                  className="edit-modal__close-modal-btn"
+                  aria-label="Fermer la modale"
+                >
+                  <RiCloseLine />
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
         {/* Error and success messages */}
         {error && <p className="error">{error}</p>}
         {success && <p className="success">{success}</p>}
-
         {/* Loading spinner */}
         <LoadingSpinner loading={loading} isOverlay={true} />
       </div>
